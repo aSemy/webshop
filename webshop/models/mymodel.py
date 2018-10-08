a04d5e9eac6ef072a5ccb76d86644986a52b43b3
Adam Semenenko <adam.semenenko@gmail.com>
2018-10-08 16:24:29 +0200

    add enum status to orders

--- webshop/models/mymodel.py
+++ webshop/models/mymodel.py
@@ -5,13 +5,22 @@ from sqlalchemy import (
     Integer,
     Text,
     DateTime,
+    Enum,
     func
 )
+
+import enum
+from enum import unique
 
 from sqlalchemy.orm import relationship
 
 from .meta import Base
 
+
+@unique
+class OrderStatus(enum.Enum):
+    CREATED = enum.auto()
+    DELIVERED = enum.auto()
 
 # # join table between orders and its components
 # TODO add this, and also add 'sale_price' and 'quantity'
@@ -20,6 +29,7 @@ from .meta import Base
 #     order_id = Column('order_id', Integer, ForeignKey('orders.id'), primary_key=True)
 #     component_id = Column('component_id', Integer, ForeignKey('components.id'), primary_key=True)
 #     component = relationship("Component")
+
 
 ordered_components_table = Table('ordered_components', Base.metadata,
                                  Column('order_id', Integer, ForeignKey('orders.id')),
@@ -32,8 +42,10 @@ class Order(Base):
     id = Column(Integer, primary_key=True)
     client_id = Column(Integer, nullable=False)
     created = Column(DateTime, default=func.now(), nullable=False)
+    status = Column(Enum(OrderStatus), default=OrderStatus.CREATED)
     preferred_delivery_datetime = Column(DateTime)
     components = relationship("Component", secondary=ordered_components_table)
+    # TODO add status enum (ordered, delivered)
 
 
 class Component(Base):

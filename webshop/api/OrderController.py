f5517d5eff78df81363450c11ba89472054be271
Adam Semenenko <adam.semenenko@gmail.com>
2018-10-08 10:55:51 +0200

    add orders endpoints. log db errors in api

--- webshop/api/OrderController.py
+++ webshop/api/OrderController.py
@@ -3,16 +3,16 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import (Component)
+from ..models import (Order)
 
 import logging
 log = logging.getLogger(__name__)
 
 
-@view_config(route_name='components', renderer='json')
+@view_config(route_name='orders', renderer='json')
 def my_view(request):
     try:
-        query = request.dbsession.query(Component)
+        query = request.dbsession.query(Order)
     except DBAPIError as e:
         log.error(str(e))
         return Response("Database error", content_type='text/plain', status=500)

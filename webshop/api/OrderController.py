a5a4897d46fb9de9861f8701a6c6fcff52051d12
Adam Semenenko <adam.semenenko@gmail.com>
2018-10-08 14:12:39 +0200

    allow creation of orders

--- webshop/api/OrderController.py
+++ webshop/api/OrderController.py
@@ -5,7 +5,12 @@ from datetime import datetime
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import (Order)
+from sqlalchemy.sql import exists
+
+from ..models import (
+    Order,
+    Component,
+)
 
 import logging
 log = logging.getLogger(__name__)
@@ -38,12 +43,40 @@ def order_create(request):
         # create order
         new_order = Order(
             client_id=request.json_body['client_id'],
-            preferred_delivery_datetime=datetime.now().replace(microsecond=0))
+            preferred_delivery_datetime=datetime.now())
 
-        # TODO validate: order's components are valid
+        # start validating
+
+        # validate components exist
+        invalid_component_ids = []
+
+
+        # request.dbsession.query(Component.id).filter(exists().where(Component.id == component_id))
+
+        # for component_id in request.json_body['component_ids']:
+        #     if not exists().where(Component.id == component_id):
+        #         invalid_component_ids.append(component_id)
+        #
+        # if len(invalid_component_ids) > 0:
+        #     raise ValueError("Component IDs do not exist: %s" % invalid_component_ids)
+
+        # finished validating
+
+        # components to add to this order
+        valid_components = []
+        # fetch components from the database
+        for component_id in request.json_body['component_ids']:
+            component = request.dbsession.query(Component).filter_by(id=component_id).first();
+            valid_components.append(component)
+
+        # add valid components to the order
+        new_order.components.extend(valid_components)
 
         request.dbsession.add(new_order)
     except DBAPIError as e:
         log.error(str(e))
         return Response("Database error", content_type='text/plain', status=500)
+    except ValueError as e:
+        log.error(str(e))
+        return Response("Component ID error", content_type='text/plain', status=500)
     return Response(status=200)
